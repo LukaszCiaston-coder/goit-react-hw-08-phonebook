073370b95ab51de1fa56653f95b790c4d211fe53
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-12-20 10:26:55 +0100

    app description

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,13 @@ export const Home = () => {
       <div className={styles.imageContainer}>
         <div className={styles.contentContainer}>
           <h2 className={styles.homeTitle}>Welcome to Phonebook App</h2>
+          <p className={styles.homeDescription}>
+            The Phonebook App is a simple and user-friendly application for
+            managing your contacts. With this app, you can easily add new
+            contacts, delate existing ones, and filter your contacts by name. It
+            provides a seamless user experience, allowing you to keep track of
+            your important contacts in one central location.
+          </p>
           <div className={styles.buttonContainer}>
             <Link to="/register" className={styles.registerButton}>
               Register
